0f9a281fb26d0f048b3b323e90fa62529813a096
wobsoriano <sorianorobertc@gmail.com>
2022-11-30 08:46:35 -0800

    update remote function args

--- src/runtime/composables/client.ts
+++ src/runtime/composables/client.ts
@@ -1,16 +1,16 @@
 import { $fetch } from 'ofetch'
 
-export function useRemoteFunction (params: string, args: any[]) {
-  return $fetch(`/api/__remote/${params}`, {
+export function useRemoteFunction (path: string, args: any[]) {
+  return $fetch(`/api/__remote/${path}`, {
     method: 'POST',
     body: {
       input: args
     },
     onResponse ({ response }) {
       if (response.status === 404) {
-        const fn = params.split('.')[1]
+        const functionName = path.split('.')[1]
         // eslint-disable-next-line no-console
-        console.error(`[nuxt-remote-fn]: Make sure ${fn} returns any data.`)
+        console.error(`[nuxt-remote-fn]: Make sure ${functionName} returns any data.`)
       }
     }
   })
