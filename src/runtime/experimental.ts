ed53229ff404acd3613cea2f6cb0506050b042ac
wobsoriano <sorianorobertc@gmail.com>
2022-12-02 20:14:07 -0800

    add comment

--- src/runtime/experimental.ts
+++ src/runtime/experimental.ts
@@ -8,6 +8,7 @@ export const useEvent = ctx.use
 
 export function wrapEventHandler (handler: EventHandler): EventHandler {
   return eventHandler(async (event) => {
+    // We read the body from here to bypass https://github.com/unjs/unctx#async-context
     const body = await readBody(event)
     event.context.__body = body
 
