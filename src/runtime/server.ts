77969ec75643d3cbe1f10a06265011f99910616c
Johann Schopplich <mail@johannschopplich.com>
2022-12-06 14:48:12 +0100

    refactor: register handler for post method only

--- src/runtime/server.ts
+++ src/runtime/server.ts
@@ -11,13 +11,6 @@ export function createRemoteFnHandler<
   M extends keyof F,
 > (functions: F): EventHandler<any> {
   return eventHandler(async (event) => {
-    if (!isMethod(event, 'POST')) {
-      throw createError({
-        statusCode: 405,
-        statusMessage: `[nuxt-remote-fn]: method "${event.node.req.method}" is not allowed.`
-      })
-    }
-
     const body = await readBody(event)
     const { moduleId, functionName } = event.context.params as {
       moduleId: M
